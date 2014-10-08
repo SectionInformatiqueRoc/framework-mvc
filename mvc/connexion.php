cf59ec75f89b5cc715ac33f38ccf9e599898f4d6
Thomas BENANCIE <benancie.thomas@gmail.com>
2014-10-08 17:18:35 +0200

    TB20141008-01 - Commentaire du code

--- mvc/connexion.php
+++ mvc/connexion.php
@@ -9,6 +9,10 @@ class Connexion {
     private function __construct() {
     }
 
+    /**
+     *  get() method get in \Blog\Params\BDD the correct username and password to the database
+     * 
+     */
     static public function get(){
         if(is_null(self::$pdo)){
             $dsn = 'mysql:dbname='.\Blog\Params\BDD::nom.';host='. \Blog\Params\BDD::host;
@@ -16,13 +20,20 @@ class Connexion {
             try {
                 self::$pdo=new \PDO($dsn, \Blog\Params\BDD::user, \Blog\Params\BDD::password);            
             } catch (PDOException $e) {
-                echo 'Connexion échouée : ' . $e->getMessage();
+                echo 'Connexion failed : ' . $e->getMessage();
             }
             self::$pdo->exec('SET NAMES \'utf8\'');
         }
         return self::$pdo;
     }
     
+    /**
+     * query() method able you to only select data from you database
+     * @param String $query SQL SELECT query
+     * @param array $params
+     * @param String $class Define the class name of the objets wich are in $rows
+     * @return array
+     */
     static public function query($query,$params,$class){
         $queryPrepare = self::get()->prepare($query);
         $queryPrepare->execute($params);
@@ -30,6 +41,12 @@ class Connexion {
         return $rows;
     }
     
+    /**
+     * exec() method able you to modify data from you database
+     * @param String $query SQL MODIFICATION query
+     * @param array $params
+     * @return String
+     */
     static public function exec($query,$params){
         $queryPrepare = self::get()->prepare($query);
         $queryPrepare->execute($params);
